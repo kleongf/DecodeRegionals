f33f1792d09b62853fab06e5743e7ce563ccab3c
amahl <amahlagrawal@gmail.com>
2026-02-15 12:29:29 -0800

    tuned pitch servo for new shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/RobotConstants.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode.robot.constants;
 
 public class RobotConstants {
-    public static double PITCH_I = Math.toRadians(29);
-    public static double PITCH_F = Math.toRadians(53);
+    public static double PITCH_I = Math.toRadians(27);
+    public static double PITCH_F = Math.toRadians(50);
     public static double PITCH_SERVO_MIN = 0.17;
     public static double PITCH_SERVO_I = 0.17;
-    public static double PITCH_SERVO_F = 0.95;
+    public static double PITCH_SERVO_F = 0.83;
     public static double LATCH_CLOSED = 0.56;
     public static double LATCH_OPEN = 0.75;
     public static String END_POSE_KEY = "END_POSE";
