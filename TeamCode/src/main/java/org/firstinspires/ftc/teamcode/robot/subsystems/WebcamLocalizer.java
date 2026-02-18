1a073913f9c89a1adfc202236dbe87a4b7a6c458
Kevin Fong <kf53307@pausd.us>
2026-02-18 11:11:11 -0800

    added webcam localizer logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/WebcamLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/WebcamLocalizer.java
@@ -11,6 +11,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.robot.constants.PoseConstants;
+import org.firstinspires.ftc.teamcode.util.decodeutil.MathUtil;
 import org.firstinspires.ftc.teamcode.util.decodeutil.Subsystem;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -33,36 +35,50 @@ public class WebcamLocalizer extends Subsystem {
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
     private Position cameraPosition = new Position(DistanceUnit.MM,
-            -122, -230, 142, 0);
+            138, 119, 236, 0);
     private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
-            0, -90, 0, 0);
+            0, -70, 0, 0);
     public static Pose toPinpointPose(Pose webcamPose) {
-        return new Pose(72 + webcamPose.getY(), 72 - webcamPose.getX(), webcamPose.getHeading());
+        return new Pose(PoseConstants.FIELD_WIDTH / 2d + webcamPose.getY(), PoseConstants.FIELD_WIDTH / 2d - webcamPose.getX(), webcamPose.getHeading());
+    }
+    public static AprilTagLibrary getDecodeTagLibraryAdjusted(){
+        return new AprilTagLibrary.Builder()
+                .addTag(20, "BlueTarget",
+                        6.5, new VectorF(-58.3727f + 1.5f, -55.6425f + 1.5f, 29.5f), DistanceUnit.INCH,
+                        new Quaternion(0.2182149f, -0.2182149f, -0.6725937f, 0.6725937f, 0))
+                .addTag(21, "Obelisk_GPP",
+                        6.5, DistanceUnit.INCH)
+                .addTag(22, "Obelisk_PGP",
+                        6.5, DistanceUnit.INCH)
+                .addTag(23, "Obelisk_PPG",
+                        6.5, DistanceUnit.INCH)
+                .addTag(24, "RedTarget",
+                        6.5, new VectorF(-58.3727f + 1.5f, 55.6425f - 1.5f, 29.5f), DistanceUnit.INCH,
+                        new Quaternion(0.6725937f, -0.6725937f, -0.2182149f, 0.2182149f, 0))
+                .build();
     }
 
     public WebcamLocalizer(HardwareMap hardwareMap) {
         currentPose = new Pose();
         aprilTag = new AprilTagProcessor.Builder()
                 // theres no way its off by that much bruh
-                .setLensIntrinsics(214.1037056, 212.72822576, 320, 240)
+                .setLensIntrinsics(920.46723598, 918.07391093, 653.71790268, 406.18310197)
                 .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
                 .setDrawTagOutline(true)
-                .setTagLibrary(AprilTagGameDatabase.getDecodeTagLibrary())
+                .setTagLibrary(getDecodeTagLibraryAdjusted())
                 .setCameraPose(cameraPosition, cameraOrientation)
                 // ... these parameters are fx, fy, cx, cy.
                 .build();
 
-        aprilTag.setDecimation(2);
-
         VisionPortal.Builder builder = new VisionPortal.Builder();
-        builder.setCameraResolution(new Size(640, 480));
+        builder.setCameraResolution(new Size(1280, 800));
 
         // Set the camera (webcam vs. built-in RC phone camera).
-        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 2"));
         // builder.enableLiveView(true);
 
         // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
 
         // Set and enable the processor.
         builder.addProcessor(aprilTag);
@@ -73,6 +89,9 @@ public class WebcamLocalizer extends Subsystem {
 
     @Override
     public void update() {
+        // PROBLEM: we can see two at once. which one do we trust?
+        // fortunately, when we relocalize based on the distance constraint, we are unable to see both
+        // so this is a non-issue
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
@@ -93,4 +112,17 @@ public class WebcamLocalizer extends Subsystem {
 
     public Pose getCurrentPose() {return currentPose;}
     public boolean getIsGoodDetection() {return isGoodDetection;}
+
+    // updates the pose but not the heading. used for normal relocalization, automatically after every 10s
+    // updates if the pose is similar
+    public Pose updatePinpointPose(Pose pinpointPose, double distanceConstraint) {
+        if (MathUtil.distance(pinpointPose, currentPose) < distanceConstraint) {
+            return new Pose(currentPose.getX(), currentPose.getY(), pinpointPose.getHeading());
+        }
+        return pinpointPose;
+    }
+
+    public Pose overridePinpointPose() {
+        return currentPose;
+    }
 }
