22bd74850e5b13420acc5675186b80b79fd5e7ad
Kevin Fong <kf53307@pausd.us>
2026-03-15 10:20:12 -0700

    somewhat tuned turret constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode2026/constants/TurretConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode2026/constants/TurretConstants.java
@@ -3,11 +3,11 @@ public class TurretConstants {
     public static final double ticksPerRevolution = 1381d;
     public static final double ticksPerRadian = ticksPerRevolution / (2 * Math.PI);
     public static final double maxPower = 0.75;
-    public static final double kP = 0.005;
+    public static final double kP = 0.01;
     public static final double kI = 0.0;
     public static final double kD = 0.0;
     public static final double kF = 0.0;
-    public static final double kV = -0.02;
+    public static final double kV = 0.03;
     public static final double kS = 0.03;
     public static final double epsilonTicks = 10;
     public static final double encoderGearRatio = 50/42d;
