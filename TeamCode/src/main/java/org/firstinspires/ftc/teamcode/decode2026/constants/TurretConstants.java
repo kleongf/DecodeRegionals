082635c3c88fd2fbcf1a4c9910be7ee9bfea7457
Kevin Fong <kf53307@pausd.us>
2026-07-06 21:23:42 -0700

    not supposed to change this lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode2026/constants/TurretConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode2026/constants/TurretConstants.java
@@ -17,7 +17,7 @@ public class TurretConstants {
     public static double gamma = 0;
     public static double encoderGearRatio = 50/42d;
     public static double encoderOffsetDegrees = -97.7;
-    public static double encoderMaxVoltage = 3.241;
+    public static double encoderMaxVoltage = 3.242;
     public static double encoderMinVoltage = 0.00;
     public static boolean useExternalEncoder = false;
     public static double nominalVoltage = 12.0;
