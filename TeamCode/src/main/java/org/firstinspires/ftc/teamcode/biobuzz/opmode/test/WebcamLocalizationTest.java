e63f7bf269d2f7285cef52d6763a3d57c56514f8
Kevin Fong <kf53307@pausd.us>
2026-08-31 00:16:22 -0700

    move webcam relocalization test to tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/biobuzz/opmode/test/WebcamLocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/biobuzz/opmode/test/WebcamLocalizationTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.biobuzz.opmode.tuning;
+package org.firstinspires.ftc.teamcode.biobuzz.opmode.test;
 
 import android.util.Size;
 
@@ -9,18 +9,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.biobuzz.constants.FieldConstants;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
-import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
